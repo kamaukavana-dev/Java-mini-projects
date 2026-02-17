f3065ccea9ac7eb518fb9d84568201ff3a3bd3f1
Daniel Maina <kamau121593@students.must.ac.ke>
2026-02-17 19:53:39 +0300

    added a java file

--- src/Overrideload.java
+++ src/Overrideload.java
@@ -1,2 +1,23 @@
+class Printer{
+    public void print(String text){
+        System.out.println(text);
+    }
+    public void print(int number){
+        System.out.println(number);
+    }
+}
+class Colorprinter extends Printer{
+    @Override
+    public void print(String text){
+        System.out.println("in color "+text );
+    }
+}
 public class Overrideload {
+    public static void main(String[] args){
+        Printer p1 = new Printer();
+        Printer p2 = new Colorprinter();
+        p1.print("Red");
+        p1.print(6);
+        p2.print("Blue");
+    }
 }
