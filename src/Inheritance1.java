2e70b70cd57cbb28dc5be0680f2ec24cb9b35fad
Daniel Maina <kamau121593@students.must.ac.ke>
2026-02-17 19:52:00 +0300

    added a java file

--- src/Inheritance1.java
+++ src/Inheritance1.java
@@ -1,2 +1,22 @@
+class Person{
+    public String name = "Daniel";
+    public int age = 19;
+    public void Introduce(){
+        System.out.println("Heeey!! My name is "+name+" and Iam "+age+" year's old.");
+    }
+}
+class Student extends Person{
+    String course = "Computer Science";
+    @Override
+    public void Introduce() {
+        System.out.println("Heeey!! My name is "+name+" and Iam "+age+" year's old,pursuing "+course+" course.");
+    }
+}
 public class Inheritance1 {
+    public static void main(String[] args){
+        Person p1 = new Person();
+        Person s1 = new Student();
+        p1.Introduce();
+        s1.Introduce();
+    }
 }
