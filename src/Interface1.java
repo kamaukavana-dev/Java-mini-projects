528bb70542be6cc61f9e646a9dc2906baa01a5b7
Daniel Maina <kamau121593@students.must.ac.ke>
2026-02-17 19:53:02 +0300

    added a java file

--- src/Interface1.java
+++ src/Interface1.java
@@ -1,2 +1,67 @@
+interface Animal{
+    public void move();
+    public void sound();
+    public void eat();
+}
+class Lion implements Animal{
+    @Override
+    public void move() {
+        System.out.println("The Lion moves on four limbs");
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("The lion roars");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("The lions feeds on flesh.");
+    }
+}
+class Cat implements Animal{
+    @Override
+    public void move() {
+        System.out.println("The Cat moves on four limbs.");
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("The Cat meows.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("The Cat feeds on milk.");
+    }
+}
+class Eagle implements Animal{
+    @Override
+    public void move() {
+        System.out.println("The Eagle flies");
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("The Eagle screech's.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("The Eagle feeds on flesh");
+    }
+}
 public class Interface1 {
+    public static void main(String[] args) {
+        Animal l1 = new Lion();
+        Animal c1 = new Cat();
+        Animal e1 = new Eagle();
+        e1.eat();
+        e1.move();
+        c1.sound();
+        c1.eat();
+        l1.sound();
+        l1.move();
+
+    }
 }
