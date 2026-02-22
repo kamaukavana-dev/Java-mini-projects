d6fa2c7e83db5c46bae5c09f62c5c38ee2c8d961
Daniel Maina <kamau121593@students.must.ac.ke>
2026-02-22 20:53:55 +0300

    added a java filehandling file

---
+++ AppendFile.java
@@ -0,0 +1,14 @@
+import java.io.FileWriter;
+import java.io.IOException;
+public class AppendFile {
+    public static void main(String[] args)  {
+        try(FileWriter writer = new FileWriter("/home/kavana-daniel/Documents/JavaNew/new.txt",true)) {
+            writer.write("So now i'm dealing with file handling in my java roadmap toward a java software dev.");
+            System.out.println("File appended successfully.");
+        }catch (IOException e) {
+            System.out.println("Error writing to file." + e.getMessage());
+
+        }
+
+    }
+}
