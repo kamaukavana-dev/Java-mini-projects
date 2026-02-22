106b5a57166024b768acdf6b2fd003ff2818bcca
Daniel Maina <kamau121593@students.must.ac.ke>
2026-02-22 20:54:22 +0300

    added a java filehandling file

---
+++ WriteFile.java
@@ -0,0 +1,14 @@
+import java.io.FileWriter;
+import java.io.IOException;
+public class WriteFile {
+    public static void main(String[] args)  {
+        try(FileWriter writer = new FileWriter("/home/kavana-daniel/Documents/JavaNew/new.txt")) {
+            writer.write("Hey there.My name is Daniel and I'm a first year student currently pursuing Bachelors in  Computer science.");
+            System.out.println("File written successfully.");
+        }catch (IOException e) {
+            System.out.println("Error writing to file." + e.getMessage());
+
+        }
+
+    }
+}
