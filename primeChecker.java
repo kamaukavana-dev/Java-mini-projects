ce80c06b595d8e7e7b1555417205114f12ea30a7
Daniel Maina <kamau121593@students.must.ac.ke>
2026-03-13 14:45:12 +0300

    Adjusted the frequency counter

--- primeChecker.java
+++ primeChecker.java
@@ -12,7 +12,7 @@ public class primeChecker {
     // Helper method to check prime
     public static boolean isPrime(int n) {
         if (n < 2) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {//we use squareroot to reduce the time it would loop fully since its basically the same
             if (n % i == 0) return false;
         }
         return true;
