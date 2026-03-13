e54b1f0c6a24cdac58eb4ec8f92684219abebf5d
Daniel Maina <kamau121593@students.must.ac.ke>
2026-03-13 14:43:39 +0300

    Adjusted the frequency counter

--- WordFrequencyCounter.java
+++ WordFrequencyCounter.java
@@ -6,8 +6,8 @@ public class WordFrequencyCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a sentence:");
-        String sentence = scanner.nextLine().toLowerCase(); // make case-insensitive
+        System.out.print("Enter a sentence:");
+        String sentence = scanner.nextLine().toLowerCase(); // make it case-insensitive
 
         // Split sentence into words
         String[] words = sentence.split(" ");
@@ -16,16 +16,16 @@ public class WordFrequencyCounter {
         Map<String, Integer> wordCount = new HashMap<>();
 
         for (String word : words) {
-            if (wordCount.containsKey(word)) {
-                wordCount.put(word, wordCount.get(word) + 1);
+            if (wordCount.containsKey(word)) {//check if we've seen the value before
+                wordCount.put(word, wordCount.get(word) + 1);//Then we increment it by 1 to add to its count
             } else {
-                wordCount.put(word, 1);
+                wordCount.put(word, 1);//if not we increment it just by one and its count will just be one
             }
         }
 
         // Print results
         System.out.println("Word frequencies:");
-        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {//we use entryset to iterate through key and value pairs in the map
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
 
