c3f34ff7984d8aeaef9c46ccb607ba75c8624d03
Daniel Maina <kamau121593@students.must.ac.ke>
2026-03-14 15:03:24 +0300

    Added a new java file

---
+++ LoggingDemo.java
@@ -0,0 +1,15 @@
+import java.util.logging.Logger;
+
+public class LoggingDemo {
+    private static final Logger logger = Logger.getLogger(LoggingDemo.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Application started"); // Info log
+        try {
+            int x = 10 / 0; // Error
+        } catch (Exception e) {
+            logger.severe("Error occurred: " + e.getMessage()); // Error log
+        }
+        logger.info("Application finished");
+    }
+}
