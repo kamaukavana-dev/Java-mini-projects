8cc65faa394fac3f48bb150a8204c8ac5e45b677
Daniel Maina <kamau121593@students.must.ac.ke>
2026-03-25 17:42:32 +0300

    modified this java file

--- Class3.java
+++ Class3.java
@@ -1,34 +1,48 @@
+//Abstract class
 abstract class Vehicle {
+    //abstract method
     public abstract void drive();
-
+    //abstract method
     public void fuel() {
         System.out.println("Vehicle needs fuel!!");
     }
 }
-
+//extending abstract class
 class Car extends Vehicle {
     @Override
     public void drive() {
         System.out.println("Car is driving....");
     }
 }
-
+//extending abstract class
 class Bike extends Vehicle {
     @Override
     public void drive() {
-        System.out.println("Bike is driving....");
+        System.out.println("Bike is riding....");
     }
 }
+//extending abstract class
+class Boat extends Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Boat is soaring....");
+    }
 
+}
+//main class
 public class Class3 {
     public static void main(String[] args) {
         Vehicle v1 = new Car();   // polymorphic reference
         Vehicle v2 = new Bike();
+        Vehicle v3 = new Boat();
 
         v1.fuel();
         v1.drive();
 
         v2.fuel();
         v2.drive();
+
+        v3.fuel();
+        v3.drive();
     }
 }
