6fb8d903cfc4a7cae6ee88cac5d284ff04a448d7
Daniel Maina <kamau121593@students.must.ac.ke>
2026-03-25 17:45:33 +0300

    modified this java file

--- Class2.java
+++ Class2.java
@@ -1,11 +1,15 @@
 class University {
     static int studentCount;
     final String UNIVERSITY_NAME = "Meru University";  // constant
-
+    //Constructor
     public University() {
         studentCount++;
     }
-
+    //Getter method
+    public String getUniversityName() {
+        return UNIVERSITY_NAME;
+    }
+    //void method
     static void showTotalStudents() {
         System.out.println("Students present: " + studentCount);
     }
@@ -16,6 +20,9 @@ public class Class2 {
         University c1 = new University();
         University c2 = new University();
         University c3 = new University();
+
+        //Get the name of the university
+        System.out.println(c3.getUniversityName());
 
 
         // Call static method using class name
