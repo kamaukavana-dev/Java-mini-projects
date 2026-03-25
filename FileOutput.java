4e7a244d9dcadf89016868be919cefeb0299e714
Daniel Maina <kamau121593@students.must.ac.ke>
2026-03-25 17:41:38 +0300

    modified this java file

--- FileOutput.java
+++ FileOutput.java
@@ -2,7 +2,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 public class FileOutput {
     public static void main(String[] args) throws IOException {
-        String text = "Hello World!";
+        String text = "Hello World,I'm a Java Developer!";
         try(FileOutputStream file = new FileOutputStream("output.txt")){
             file.write(text.getBytes());//convert the text to bytes
             System.out.println("File Write Success!");
