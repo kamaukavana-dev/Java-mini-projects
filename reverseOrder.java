9c2c4637dbef8ab68b9914de60bb128888776af4
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-05 20:34:06 +0300

    Modified this java file

--- reverseOrder.java
+++ reverseOrder.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class reverseOrder {
     public static void main(String[] args){
-        int[] myArray = {1, 2, 3, 4, 5};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
 
+        int[] myArray = new int[n];
+        System.out.print("Enter "+n+" elements: ");
+        for(int i=0;i<n;i++){
+            myArray[i] = sc.nextInt();
+        }
+        System.out.println("REVERSED ARRAY ELEMENT");
         // Start from the last index (length - 1) down to 0
         for(int i = myArray.length - 1; i >= 0; i--){
             System.out.println(myArray[i]);
