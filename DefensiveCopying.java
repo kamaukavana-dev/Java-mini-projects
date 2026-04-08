b57abe67c9b822e7a87f2ccec30ef368b85e6c73
Daniel Maina <kamau121593@students.must.ac.ke>
2026-04-08 22:02:16 +0300

    Modified this java file

--- DefensiveCopying.java
+++ DefensiveCopying.java
@@ -7,9 +7,5 @@ public class DefensiveCopying {
         this.myArr = Arrays.copyOf(myArr, myArr.length);//Defensive copying: creates a copy of the input array to prevent external modifications from affecting the internal state of the object
     }
 
-    private int [] getMyArr(){
-        return Arrays.copyOf(myArr, myArr.length);//Defensive copying out
-    }
-    public static void main(String[] args){
-        DefensiveCopying c = new DefensiveCopying(new int[]{1,2,3});
+
 
