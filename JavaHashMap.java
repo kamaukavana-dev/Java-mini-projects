7b319fc4cb80e6d15d3ac7ba5152d5824a3317fc
Daniel Maina <kamau121593@students.must.ac.ke>
2026-09-08 22:21:47 +0300

    Hashmap modification

--- JavaHashMap.java
+++ JavaHashMap.java
@@ -3,14 +3,4 @@ import java.util.function.Predicate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmployeeReport {
 
-    public static List<Employee> filterEmployees(List<Employee> list, Predicate<Employee> filter) {
-        List<Employee> result = new ArrayList<>();
-        for (Employee e : list) {
-            if (filter.test(e)) {
-                result.add(e);
-            }
-        }
-        return result;
-    }
