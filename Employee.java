179d0237ce1d4788b0ab491aee79607a0ca95b73
Daniel Maina <kamau121593@students.must.ac.ke>
2026-09-15 22:09:29 +0300

    Employee class

--- Employee.java
+++ Employee.java
@@ -1,8 +1,37 @@
 public class Employee {
 
+    private String name;
+    private String department;
+    private double salary;
+    private int yearsExperience;
 
+    public Employee(String name, String department, double salary, int yearsExperience) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.yearsExperience = yearsExperience;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getDepartment() {
+        return department;
+    }
 
+    public double getSalary() {
+        return salary;
+    }
 
+    public int getYearsExperience() {
+        return yearsExperience;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + department + " | $" + salary + " | "
+                + yearsExperience + " years";
+    }
+}
 
