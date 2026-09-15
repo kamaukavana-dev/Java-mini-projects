3cf5a51173e8c561dece8dd1a393694e06f659c6
Daniel Maina <kamau121593@students.must.ac.ke>
2026-09-15 22:08:03 +0300

    Use of Constructors

--- Employee.java
+++ Employee.java
@@ -5,12 +5,6 @@ public class Employee {
     private double salary;
     private int yearsExperience;
 
-    public Employee(String name, String department, double salary, int yearsExperience) {
-        this.name = name;
-        this.department = department;
-        this.salary = salary;
-        this.yearsExperience = yearsExperience;
-    }
 
 
 
