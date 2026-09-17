45c19908282542f9aa4c692de3ec9d08d8fa686a
Daniel Maina <kamau121593@students.must.ac.ke>
2026-09-17 22:41:00 +0300

    Unicode escape sequence

--- Unicode.java
+++ Unicode.java
@@ -5,14 +5,6 @@ public class Unicode {
 
     public static void main(String[] args) {
 
-        // --------------------------------------------------
-        // 1. Unicode escape sequences
-        // --------------------------------------------------
-        char romanEight = '\u2177';
-
-        System.out.println("1. Unicode escape:");
-        System.out.println("Character: " + romanEight);
-        System.out.printf("Code point: U+%04X%n", (int) romanEight);
 
 
 
