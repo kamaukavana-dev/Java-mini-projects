f6cb1143a278463afcc8855e199b8b7ea6123b5c
Daniel Maina <kamau121593@students.must.ac.ke>
2026-09-22 09:37:00 +0300

    Inner while loop fix

--- guessingGame.java
+++ guessingGame.java
@@ -30,6 +30,8 @@ public class guessingGame {
                     System.out.println("You are nearly there...");
                 } else if (num > lucky_num) {
                     System.out.println("Your number was too high!!!");
+                } else if (num < lucky_num) {
+                        System.out.println("Your number was too Low!!!");
                 } else {
                     System.out.println("Your number was too low!!");
                 }
