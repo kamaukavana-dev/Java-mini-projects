821b2bd1682729f1a4090a1bb14679661873d26f
Daniel Maina <kamau121593@students.must.ac.ke>
2026-09-25 22:09:15 +0300

    Arrays in java

--- Dante1.java
+++ Dante1.java
@@ -1,5 +1,6 @@
 public class Dante1 {
-    public static void main(String[] args){
-        System.out.println("Hello there");
-    }
+    // With new
+    String[] cars = new String[] {"Volvo", "BMW", "Ford", "Mazda"};
+
+
 }
